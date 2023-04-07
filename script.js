8b7e013841703e14e27880de89a4c0beac0955ba
Caio Nascimento <caionsantos2000@gmail.com>
2023-04-07 14:31:09 -0300

    fix: send email function

--- script.js
+++ script.js
@@ -1,31 +1,50 @@
-//import { EMAIL, PASSWORD } from './env.js'
+const getValueFromElement = (elementId) => {
+  return document.getElementById(elementId).value;
+}
 
-const sendEmail = async(event) => {
-  event.preventDefault();
-  
-  let name = document.getElementById('nome').value
-  let email = document.getElementById('email').value
-  let texto = document.getElementById('textArea').value
-
+const sendEmail = async () => {
+  const name = getValueFromElement('nome');
+  const email = getValueFromElement('email');
+  const message = getValueFromElement('textArea');
   try {
-    Email.send({  
-      Host: "smtp.elasticemail.com",
-      Port: "2525",
-      Username : process.env.EMAIL,
-      Password : process.env.PASSWORD,
-      To : EMAIL,
-      From : EMAIL,
-      Subject : "email enviado do meu portfolio",
-      Body : `Oi Caio, me chamo ${name}, meu email é ${email}. ${texto}`,
-      })
-    alert("Email enviado com sucesso")
+    const res = await Email.send({
+      Host: 'smtp.elasticemail.com',
+      Port: '2525',
+      Username: process.env.EMAIL,
+      Password: process.env.PASSWORD,
+      To: email,
+      From: process.env.EMAIL,
+      Subject: 'email enviado do meu portfolio',
+      Body: `Oi Caio, me chamo ${name}, meu email é ${email}. <br> ${message}`
+    })
+    if (res.includes('addresses')) {
+      throw new Error('Favor preencha seu email')
+    }
+    if (res !== 'OK') {
+      throw new Error(res);
+    }
+
+    return res
   } catch (error) {
-    console.log(error)
-    alert("Falha no envio")
+    throw error
   }
 }
 
-document.getElementById('button').addEventListener(
-  'click', sendEmail
-);
+const submitForm = async () => {
+  try {
+    await sendEmail();
+    alert('Email enviado com sucesso');
+  } catch (error) {
+    console.error(error.message)
+    if (error.message === 'Favor preencha seu email') {
+      alert(error.message)
+      return
+    }
+    alert('Atualmente, não estamos conseguindo enviar e-mails. \n Por favor, contate-me pelo WhatsApp');
+  }
+}
 
+document.getElementById('form-content').addEventListener('submit', (event) => {
+  event.preventDefault();
+  submitForm();
+});
